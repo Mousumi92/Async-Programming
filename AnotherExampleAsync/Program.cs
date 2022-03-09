40b61ca6e2589c1e0380809dc67cae9929c0512c
Mousumi Kumari <mousumi.kumari@modmed.com>
2022-03-09 09:41:00 +0530

    async programming

---
+++ AnotherExampleAsync/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AnotherExampleAsync
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await ExecuteAsyncFunctions();
+            Console.ReadKey();
+        }
+
+        public static async Task ExecuteAsyncFunctions()
+        {
+            var firstAsync = FirstAsync();
+            var secondAsync = SecondAsync();
+            var thirdAsync = ThirdAsync();
+            await Task.WhenAll(firstAsync, secondAsync, thirdAsync);
+        }
+
+        public static async Task FirstAsync()
+        {
+            Console.WriteLine("First Async Method on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+            await Task.Delay(1000);
+            Console.WriteLine("First Async Method Continuation on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+        }
+        public static async Task SecondAsync()
+        {
+            Console.WriteLine("Second Async Method on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+            await Task.Delay(1000);
+            Console.WriteLine("Second Async Method Continuation on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+        }
+        public static async Task ThirdAsync()
+        {
+            Console.WriteLine("Third Async Method on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+            await Task.Delay(1000);
+            Console.WriteLine("Third Async Method Continuation on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
